491d961c3bc1739a205482a1617bb38290cda3b2
Khalil <ka10@sanger.ac.uk>
2021-01-21 11:33:17 +0000

    Create iso-3166-codes.json

---
+++ generate.js
@@ -0,0 +1,37 @@
+const axios = require("axios");
+const Papaparse = require("papaparse");
+
+function fetchCsv(csv) {
+  return new Promise((resolve, reject) => {
+    Papaparse.parse(
+      csv,
+      {
+        complete: resolve,
+        error: reject,
+      },
+    );
+  });
+}
+
+async function main() {
+  const res = await axios("https://docs.google.com/spreadsheets/d/e/2PACX-1vQt0l5CF2hyN3ajkVS_Lx5zPwi0eZ1bD675FPKyt2tKoYPkNynfSLYw6WOi_j0MQTgSsazlfrxOrGtP/pub?gid=0&single=true&output=csv");
+  const csv = Papaparse.parse(res.data);
+  // remove header row
+  csv.data.shift();
+  const lines = [];
+  for (const row of csv.data) {
+    // remove type column
+    row.pop();
+    if (row[1] && row[2]) {
+      row[1] = parseFloat(row[1]);
+      row[2] = parseFloat(row[2]);
+      lines.push(row);
+    }
+  }
+
+  return JSON.stringify(lines);
+}
+
+main()
+  .then(console.log)
+  .catch(console.error);
